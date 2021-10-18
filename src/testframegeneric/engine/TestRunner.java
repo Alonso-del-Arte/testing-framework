d75b226ab381ce4ee42c6293038bdda44c3864da
Alonso del Arte <alonso.delarte@gmail.com>
2021-10-17 22:45:51 -0400

    First failing test through ad hoc test runner

--- src/testframegeneric/engine/TestRunner.java
+++ src/testframegeneric/engine/TestRunner.java
@@ -1,13 +1,23 @@
-/**
- * 
- */
 package testframegeneric.engine;
+
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 
  * @author Alonso del Arte
  */
 public class TestRunner {
+	
+	public static TestResult run(Method test) {
+		return null;
+	}
+	
+	public static Set<TestResult> run(Object testClass) {
+		Set<TestResult> results = new HashSet<TestResult>();
+		return results;
+	}
 
 	/**
 	 * @param args
--- test/testframegeneric/engine/TestRunnerTest.java
+++ test/testframegeneric/engine/TestRunnerTest.java
@@ -1,5 +1,57 @@
 package testframegeneric.engine;
 
+import java.util.Set;
+
+import testframegeneric.api.Test;
+import static testframegeneric.api.Asserters.*;
+
 public class TestRunnerTest {
+	
+	public static void checkResultSetSizeIsPositive(Set<TestResult> results) {
+		int size = results.size();
+		if (size == 0) {
+			String msg = "TestRunner failed to pick up any tests";
+			throw new AssertionError(msg);
+		} else {
+			System.out.println("TestRunner picked up " + size + " tests");
+		}
+	}
+	
+	public static void main(String[] args) {
+		Set<TestResult> results = TestRunner.run(new TestRunnerTest.ToyTests());
+		checkResultSetSizeIsPositive(results);
+	}
+	
+	public static class ToyTests {
+		
+		private static boolean relay(boolean flag) {
+			return flag;
+		}
+		
+		@Test
+		public void testThatShouldPass() {
+			String msg = "This test should be reported as passing";
+			System.out.println(msg);
+			assert relay(true) : msg;
+		}
+		
+		@Test
+		public void testThatShouldFail() {
+			String msg = "This test should be reported as failing";
+			System.out.println(msg);
+			assert relay(false) : msg;
+		}
+		
+		// TODO: Write test that should be skipped
+		
+		@Test
+		public void testThatShouldCauseError() {
+			String msg 
+			        = "This test should be reported as having caused an error";
+			System.out.println(msg);
+			throw new RuntimeException(msg);
+		}
+		
+	}
 
 }
