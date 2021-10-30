e85cc7421812c5f6892b92c5251f7227ca0be3c8
Alonso del Arte <alonso.delarte@gmail.com>
2021-10-30 00:59:01 -0400

    Oops, can't check invocation counts that way

--- test/testframe/engine/ToyTests.java
+++ test/testframe/engine/ToyTests.java
@@ -8,58 +8,22 @@ import testframe.api.Test;
 
 public class ToyTests {
     
-    static String status = "The ToyTests class has been loaded";
-    
-    static final String BEFORE_ALL_STATUS = "setUpClass() has run";
-
-    static final String BEFORE_EACH_STATUS = "setUp() has run";
-    
-    static final String DURING_STATUS = "A test is running";
-
-    static final String AFTER_EACH_STATUS = "tearDown() has run";
-
-    static final String AFTER_ALL_STATUS = "tearDownClass() has run";
-    
-    static int beforeAllCounter = 0;
-    
-    static int beforeEachCounter = 0;
-    
-    static int duringCounter = 0;
-    
-    static int afterEachCounter = 0;
-    
-    static int afterAllCounter = 0;
-
     private static boolean relay(boolean flag) {
         return flag;
     }
     
-    private static void reportStatus() {
-        System.out.println(status);
-    }
-    
     @BeforeAllTests
     public void setUpClass() {
-        beforeAllCounter++;
-        status = BEFORE_ALL_STATUS;
-        reportStatus();
+        // TODO: Figure out how to report that this was invoked
     }
     
     @BeforeEachTest
     public void setUp() {
-        String msg = "setUpClass() should have run once already";
-        assert beforeAllCounter == 1 : msg;
-        beforeEachCounter++;
-        status = BEFORE_EACH_STATUS;
-        reportStatus();
+        // TODO: Figure out how to report that this was invoked
     }
     
     private void recordTestIsRunning() {
-        duringCounter++;
-        String msg = "setUp() should've run as many times as tests run so far";
-        assert duringCounter == beforeEachCounter : msg;
-        status = DURING_STATUS;
-        reportStatus();
+        // TODO: Figure out how to report that this was invoked
     }
     
     @Test
@@ -73,7 +37,6 @@ public class ToyTests {
     @Test
     public void testThatShouldFail() {
         recordTestIsRunning();
-        status = DURING_STATUS;
         String msg = "This test should be reported as failing";
         System.out.println(msg);
         assert relay(false) : msg;
@@ -84,7 +47,6 @@ public class ToyTests {
     @Test
     public void testThatShouldCauseError() {
         recordTestIsRunning();
-        status = DURING_STATUS;
         String msg 
                 = "This test should be reported as having caused an error";
         System.out.println(msg);
@@ -93,18 +55,12 @@ public class ToyTests {
     
     @AfterEachTest
     public void tearDown() {
-        afterEachCounter++;
-        String msg = "tearDown() should've run once for each test so far";
-        assert duringCounter == afterEachCounter : msg;
-        status = AFTER_EACH_STATUS;
-        reportStatus();
+        // TODO: Figure out how to report that this was invoked
     }
     
     @AfterAllTests
     public void tearDownClass() {
-        afterAllCounter++;
-        status = AFTER_ALL_STATUS;
-        reportStatus();
+        // TODO: Figure out how to report that this was invoked
     }
     
 }
