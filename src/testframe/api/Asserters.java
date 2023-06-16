ca0917d893dd2691d7ceb79fe3f4def8fc71bfa5
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-16 02:16:10 -0400

    Rewinding assert equals for int[] to failing

--- src/testframe/api/Asserters.java
+++ src/testframe/api/Asserters.java
@@ -214,15 +214,15 @@ public class Asserters {
     }
     
     public static void assertEquals(int[] expected, int[] actual, String msg) {
-// if (expected.length != actual.length) {
-            String errMsg = msg + ". Arrays differ in length: expected has " 
-                    + expected.length + " elements but actual has " 
-                    + actual.length + " elements";
-            if (errMsg.startsWith(". ")) {
-                errMsg = errMsg.substring(2);
-            }
-            throw new AssertionError(errMsg);
-// }
+//// if (expected.length != actual.length) {
+//            String errMsg = msg + ". Arrays differ in length: expected has " 
+//                    + expected.length + " elements but actual has " 
+//                    + actual.length + " elements";
+//            if (errMsg.startsWith(". ")) {
+//                errMsg = errMsg.substring(2);
+//            }
+//            throw new AssertionError(errMsg);
+//// }
     }
     
     // TODO: Write tests for this
