a83a623d941fdf610ffd3474c0b03d6e41afca90
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-18 22:16:16 -0400

    Changed to annotation

--- src/testframe/api/Skip.java
+++ src/testframe/api/Skip.java
@@ -1,6 +1,18 @@
 package testframe.api;
 
-//TODO: Turn this into an annotation
-public class Skip {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates that a test annotated with {@link Test} should be skipped. Note 
+ * that this annotation has no effect on a procedure that does not also have the 
+ * <code>@Test</code> annotation.
+ * @author Alonso del Arte
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Skip {
 
 }
