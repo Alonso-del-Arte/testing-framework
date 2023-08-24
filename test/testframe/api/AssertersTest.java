8c111978fbd94c74965ca95dddf78549beafa4ed
Alonso del Arte <alonso.delarte@gmail.com>
2023-08-24 17:44:24 -0400

    Failing assert positive but it is negative default message test

--- test/testframe/api/AssertersTest.java
+++ test/testframe/api/AssertersTest.java
@@ -1971,6 +1971,26 @@ public class AssertersTest {
     }
     
     @Test
+    public void testAssertPositiveButItIsNegativeDefaultMessage() {
+        long number = RANDOM.nextLong() | Long.MIN_VALUE;
+        boolean failOccurred = false;
+        try {
+            Asserters.assertPositive(number);
+        } catch (AssertionError ae) {
+            failOccurred = true;
+            String expected = "Number " + number 
+                    + " expected to be greater than 0";
+            String actual = ae.getMessage();
+            String msg = "Expected \"" + expected + "\" but was \"" + actual 
+                    + "\"";
+            assert expected.equals(actual) : msg;
+        }
+        String msg = "Asserting that number " + number 
+                + " is positive should have failed the test";
+        assert failOccurred : msg;
+    }
+    
+    @Test
     public void testAssertPositiveDoubleButItIsNot() {
         int floor = RANDOM.nextInt() | Integer.MIN_VALUE;
         double number = floor + RANDOM.nextDouble();
