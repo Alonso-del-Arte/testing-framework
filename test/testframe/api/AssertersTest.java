1b92ef374a9c931992a4c1d424475818231aa073
Alonso del Arte <alonso.delarte@gmail.com>
2023-08-29 01:55:59 -0400

    Failing positive zero, negative zero are not positive tests

--- test/testframe/api/AssertersTest.java
+++ test/testframe/api/AssertersTest.java
@@ -1952,6 +1952,36 @@ public class AssertersTest {
     }
     
     @Test
+    public void testAssertPositiveZeroIsNotPositive() {
+        double number = 0.0;
+        boolean failOccurred = false;
+        try {
+            Asserters.assertNotPositive(number, EXAMPLE_ASSERTION_MESSAGE_PART);
+        } catch (AssertionError ae) {
+            failOccurred = true;
+            System.out.println("\"" + ae.getMessage() + "\"");
+        }
+        String msg = "Asserting that number " + number 
+                + " is not positive should not have failed the test";
+        assert !failOccurred : msg;
+    }
+    
+    @Test
+    public void testAssertNegativeZeroIsNotPositive() {
+        double number = Double.longBitsToDouble(Long.MIN_VALUE);
+        boolean failOccurred = false;
+        try {
+            Asserters.assertNotPositive(number, EXAMPLE_ASSERTION_MESSAGE_PART);
+        } catch (AssertionError ae) {
+            failOccurred = true;
+            System.out.println("\"" + ae.getMessage() + "\"");
+        }
+        String msg = "Asserting that number " + number 
+                + " is not positive should not have failed the test";
+        assert !failOccurred : msg;
+    }
+    
+    @Test
     public void testAssertPositiveButItIsNegative() {
         long number = RANDOM.nextLong() | Long.MIN_VALUE;
         boolean failOccurred = false;
