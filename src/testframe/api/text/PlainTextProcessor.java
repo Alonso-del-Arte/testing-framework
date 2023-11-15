c6bc9a8abe9948efcddf9dd6be74b0e395a485b1
Alonso del Arte <alonso.delarte@gmail.com>
2023-11-14 22:55:37 -0500

    New package

---
+++ src/testframe/api/text/PlainTextProcessor.java
@@ -0,0 +1,15 @@
+package testframe.api.text;
+
+public class PlainTextProcessor {
+    
+    // TODO: Write tests for this
+    public static String collapseWhitespace(String s) {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+    
+    // TODO: Write tests for this
+    public static boolean equalsWithWhitespaceCollapsed(String s1, String s2) {
+        return false;
+    }
+
+}
---
+++ test/testframe/api/text/HTMLProcessorTest.java
@@ -0,0 +1,8 @@
+package testframe.api.text;
+
+import testframe.api.Test;
+import static testframe.api.Asserters.*;
+
+public class HTMLProcessorTest {
+
+}
