45705c74179ed384ec125407a9c662ab55cb9d90
Alonso del Arte <alonso.delarte@gmail.com>
2023-11-15 23:29:14 -0500

    Put in a test placeholder

--- test/testframe/api/text/HTMLProcessorTest.java
+++ test/testframe/api/text/HTMLProcessorTest.java
@@ -4,5 +4,10 @@ import testframe.api.Test;
 import static testframe.api.Asserters.*;
 
 public class HTMLProcessorTest {
+    
+    @Test
+    public void placeholder() {
+        fail("THIS IS A PLACEHOLDER FOR A TEST");
+    }
 
 }
