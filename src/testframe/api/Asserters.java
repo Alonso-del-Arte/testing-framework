39de195bf154a6efe5d8bd4b8abc5b739ffbc07d
Alonso del Arte <alonso.delarte@gmail.com>
2023-12-28 17:54:07 -0500

    Passing assert double[]s are equal but they're not, default variance, test

--- src/testframe/api/Asserters.java
+++ src/testframe/api/Asserters.java
@@ -280,7 +280,11 @@ public class Asserters {
     // TODO: Write tests for this
     public static void assertEquals(double[] expected, double[] actual, 
             String msg) {
-         assertEquals(expected, actual, Math.PI, "msg");
+        int expLen = expected.length;
+        int actLen = actual.length;
+        String lenMsg = msg + ". Arrays differ in length: expected has " 
+                + expLen + " elements but actual has " + actLen + " elements";
+        assert expLen == actLen : lenMsg;
     }
     
     /**
