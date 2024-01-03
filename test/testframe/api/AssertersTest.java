05a58736c40f52f1cb269de5acb068c0eaeb890e
Alonso del Arte <alonso.delarte@gmail.com>
2024-01-03 15:23:10 -0500

    Removed extra letter

--- test/testframe/api/AssertersTest.java
+++ test/testframe/api/AssertersTest.java
@@ -4476,7 +4476,7 @@ public class AssertersTest {
         } catch (AssertionError ae) {
             failOccurred = true;
             String expected = EXAMPLE_ASSERTION_MESSAGE_PART 
-                    + ". Expected list to contasin " + listAStr 
+                    + ". Expected list to contain " + listAStr 
                     + " but actually contained " + listBStr;
             String actual = ae.getMessage();
             String msg = "Expected \"" + expected + "\" but was \"" + actual 
