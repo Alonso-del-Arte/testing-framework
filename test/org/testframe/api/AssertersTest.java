a66f8da697198c355af45c3ccbcd784b3002a7ef
Alonso del Arte <alonso.delarte@gmail.com>
2024-01-19 23:28:28 -0500

    Failing assert Comparable is in range test

--- test/org/testframe/api/AssertersTest.java
+++ test/org/testframe/api/AssertersTest.java
@@ -4040,6 +4040,33 @@ public class AssertersTest {
     }
     
     @Test
+    public void testAssertInRangeComparable() {
+        int radix = 36;
+        int size = RANDOM.nextInt(32) + 8;
+        String[] strings = new String[size];
+        for (int i = 0; i < size; i++) {
+            int num = RANDOM.nextInt() + i;
+            strings[i] = Integer.toString(num, radix);
+        }
+        Arrays.sort(strings);
+        String minimum = strings[0];
+        String maximum = strings[size - 1];
+        for (String actual : strings) {
+            boolean failOccurred = false;
+            try {
+                Asserters.assertInRange(minimum, actual, maximum, 
+                        EXAMPLE_ASSERTION_MESSAGE_PART);
+            } catch (AssertionError ae) {
+                failOccurred = true;
+            }
+            String msg = "Asserting that \"" + actual 
+                    + "\" is in the range from \"" + minimum + "\" to \"" 
+                    + maximum + "\" should not have failed the test";
+            assert !failOccurred : msg;
+        }
+    }
+    
+    @Test
     public void testAssertDifferentButIsSame() {
         int number = RANDOM.nextInt();
         boolean failOccurred = false;
