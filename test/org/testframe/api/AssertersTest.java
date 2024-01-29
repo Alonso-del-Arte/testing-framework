ac217d9eb9ff76ef042025c8607c699dfca8e65b
Alonso del Arte <alonso.delarte@gmail.com>
2024-01-29 00:57:04 -0500

    Failing assert double in range rejects NaN for maximum test

--- test/org/testframe/api/AssertersTest.java
+++ test/org/testframe/api/AssertersTest.java
@@ -4033,6 +4033,29 @@ public class AssertersTest {
         assert exceptionOccurred : msg;
     }
     
+    @Test
+    public void testNaNNotAllowedForAssertDoubleInRangeMaximum() {
+        double minimum = -1.0 + RANDOM.nextDouble();
+        double maximum = Double.NaN;
+        double number = RANDOM.nextDouble();
+        boolean exceptionOccurred = false;
+        try {
+            Asserters.assertInRange(minimum, number, maximum, LOCAL_DELTA, 
+                    EXAMPLE_ASSERTION_MESSAGE_PART);
+        } catch (IllegalArgumentException iae) {
+            exceptionOccurred = true;
+            String expected = "Minimum, maximum must both not be NaN";
+            String actual = iae.getMessage();
+            String msg = "Expected \"" + expected + "\" but was \"" + actual 
+                    + "\"";
+            assert expected.equals(actual) : msg;
+        } catch (AssertionError ae) {
+            System.out.println("\"" + ae.getMessage() + "\"");
+        }
+        String msg = "Using NaN for maximum should have caused exception";
+        assert exceptionOccurred : msg;
+    }
+    
     // TODO: Write more tests for assertInRange involving double
     
     @Test
