4b9d559e91a8f58b8d6019a23653d4c4a2b3ca98
Alonso del Arte <alonso.delarte@gmail.com>
2024-02-16 23:46:46 -0500

    Failing assert double in range but is below, default delta, test

--- test/org/testframe/api/AssertersTest.java
+++ test/org/testframe/api/AssertersTest.java
@@ -4253,6 +4253,32 @@ public class AssertersTest {
         assert exceptionOccurred : msg;
     }
     
+    @Test
+    public void testAssertInRangeDoubleButIsBelowDefaultDelta() {
+        double minimum = -1.0 + RANDOM.nextDouble();
+        double maximum = 1.0 + RANDOM.nextDouble();
+        double belowRange = -2.0 - RANDOM.nextDouble() 
+                - Asserters.DEFAULT_TEST_DELTA;
+        boolean failOccurred = false;
+        try {
+            Asserters.assertInRange(minimum, belowRange, maximum, 
+                    EXAMPLE_ASSERTION_MESSAGE_PART);
+        } catch (AssertionError ae) {
+            failOccurred = true;
+            String expected = "Expected " + belowRange + " to be in range from " 
+                    + minimum + " to " + maximum + " with variance " 
+                    + Asserters.DEFAULT_TEST_DELTA;
+            String actual = ae.getMessage();
+            String msg = "Expected \"" + expected + "\" but was \"" + actual 
+                    + "\"";
+            assert expected.equals(actual) : msg;
+        }
+        String msg = "The number " + belowRange 
+                + " should not be said to be in range " + minimum + " to " 
+                + maximum;
+        assert failOccurred : msg;
+    }
+    
     // TODO: Write more tests for assertInRange involving double
     
     @Test
