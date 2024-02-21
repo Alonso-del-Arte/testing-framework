606f0ebe8d6e4bb15dec532a899bd6556611fb3f
Alonso del Arte <alonso.delarte@gmail.com>
2024-02-21 13:32:14 -0500

    Passing assert double in range but is above, default message, test

--- src/org/testframe/api/Asserters.java
+++ src/org/testframe/api/Asserters.java
@@ -1149,7 +1149,7 @@ public class Asserters {
             String excMsg = "Minimum, maximum, delta should not be NaN";
             throw new IllegalArgumentException(excMsg);
         }
-        if (actual < minimum) {
+        if (actual < minimum || maximum < actual) {
             String errMsg = "Expected " + actual + " to be in range from " 
                     + minimum + " to " + maximum + " with variance " + delta;
             throw new AssertionError(errMsg);
