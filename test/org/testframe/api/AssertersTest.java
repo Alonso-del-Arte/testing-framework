fdc2bf72cb3db6826d491ed7b026c5d91f801b1e
Alonso del Arte <alonso.delarte@gmail.com>
2024-02-29 01:47:24 -0500

    Failing assert E arrays are different but are actually same, default message, test

--- test/org/testframe/api/AssertersTest.java
+++ test/org/testframe/api/AssertersTest.java
@@ -5380,6 +5380,35 @@ public class AssertersTest {
         assert !failOccurred : msgB;
     }
     
+    @Test
+    public void testAssertDifferentObjectArraysButAreSameDefaultMessage() {
+        int length = RANDOM.nextInt(8) + 2;
+        Color[] arrayA = new Color[length];
+        Color[] arrayB = new Color[length];
+        for (int i = 0; i < length; i++) {
+            Color color = new Color(RANDOM.nextInt());
+            arrayA[i] = color;
+            arrayB[i] = color;
+        }
+        boolean failOccurred = false;
+        try {
+            Asserters.assertDifferent(arrayA, arrayB);
+        } catch (AssertionError ae) {
+            failOccurred = true;
+            String expected = "Arrays " + Arrays.toString(arrayA) + " and " 
+                    + Arrays.toString(arrayB) 
+                    + " are not different as asserted";
+            String actual = ae.getMessage();
+            String msg = "Expected \"" + expected + "\" but was \"" + actual 
+                    + "\"";
+            assert expected.equals(actual) : msg;
+        }
+        String msg = "Asserting " + Arrays.toString(arrayA) + " and " 
+                + Arrays.toString(arrayB) 
+                + " are different should have failed the test";
+        assert failOccurred : msg;
+    }
+    
     // TODO: Write more tests for assertDifferent() on arrays
     
     @Test
