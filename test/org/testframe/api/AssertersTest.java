15459bf75f73ccf0112691a877fd07f3442591c5
Alonso del Arte <alonso.delarte@gmail.com>
2024-03-25 23:56:19 -0400

    Failing assert same length int arrays differ in one element

--- test/org/testframe/api/AssertersTest.java
+++ test/org/testframe/api/AssertersTest.java
@@ -5324,6 +5324,31 @@ public class AssertersTest {
         assert !failOccurred : msg;
     }
     
+    @Test
+    public void testAssertDifferentIntArraysSameLengthDifferInOneElement() {
+        int length = RANDOM.nextInt(8) + 2;
+        int[] some = new int[length];
+        int[] other = new int[length];
+        for (int i = 0; i < length; i++) {
+            int number = RANDOM.nextInt() + i;
+            some[i] = number;
+            other[i] = number;
+        }
+        int changeIndex = RANDOM.nextInt(length);
+        other[changeIndex]++;
+        boolean failOccurred = false;
+        try {
+            Asserters.assertDifferent(some, other, 
+                    EXAMPLE_ASSERTION_MESSAGE_PART);
+        } catch (AssertionError ae) {
+            failOccurred = true;
+        }
+        String msg = "Asserting " + Arrays.toString(some) + " and " 
+                + Arrays.toString(other) 
+                + " are different should not have failed the test";
+        assert !failOccurred : msg;
+    }
+    
     // TODO: Write more tests for assertDifferent() on arrays
     
     @Test
