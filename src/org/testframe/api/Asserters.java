249c60a16d9b8435989e3595115bfc33e0589f5f
Alonso del Arte <alonso.delarte@gmail.com>
2024-03-28 01:24:43 -0400

    Passing assert double[]s different and they do differ in length, default delta, default message, test

--- src/org/testframe/api/Asserters.java
+++ src/org/testframe/api/Asserters.java
@@ -1491,12 +1491,14 @@ public class Asserters {
         }
     }
     
-    // TODO: Write tests for this
     public static void assertDifferent(double[] some, double[] other) {
-        String errMsg = "Arrays " + Arrays.toString(some) + " and " 
-                + Arrays.toString(other) + " are not different beyond variance " 
-                + DEFAULT_TEST_DELTA + " as asserted";
-        throw new AssertionError(errMsg);
+        if (some.length == other.length) {
+            String errMsg = "Arrays " + Arrays.toString(some) + " and " 
+                    + Arrays.toString(other) 
+                    + " are not different beyond variance " + DEFAULT_TEST_DELTA 
+                    + " as asserted";
+            throw new AssertionError(errMsg);
+        }
     }
     
     public static void assertDifferent(double[] some, double[] other, 
