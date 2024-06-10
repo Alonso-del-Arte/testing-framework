413ab0ea8eacaf15893333c549b1c5dda094e51a
Alonso del Arte <alonso.delarte@gmail.com>
2024-06-10 17:25:02 -0400

    Some Javadoc

--- src/org/testframe/api/random/CoinSide.java
+++ src/org/testframe/api/random/CoinSide.java
@@ -1,7 +1,21 @@
 package org.testframe.api.random;
 
+/**
+ * Represents the two sides of a coin, heads or tails.
+ * @author Alonso del Arte
+ */
 public enum CoinSide {
     
-    HEADS, TAILS
+    /**
+     * The heads side, or obverse, of a coin. May be mapped to either true or 
+     * false, as needed.
+     */
+    HEADS, 
+    
+    /**
+     * The tails side, or reverse, of a coin. May be mapped to either true or 
+     * false, as needed.
+     */
+    TAILS
 
 }
--- src/org/testframe/api/random/ExpandedRandom.java
+++ src/org/testframe/api/random/ExpandedRandom.java
@@ -9,16 +9,55 @@ public abstract class ExpandedRandom extends Random {
 
     private static final long serialVersionUID = 4553887127401760416L;
     
+    /**
+     * Gives a pseudorandom power of two.
+     * @return A power of two. One of 1, 2, 4, 8, 16, 32, 64, 128, ..., 
+     * 1073741824.
+     */
     public abstract int nextPowerOfTwo();
     
+    /**
+     * Flips a coin pseudorandomly.
+     * @return Either {@link CoinSide#HEADS} or {@link CoinSide#TAILS}.
+     */
     public abstract CoinSide flipCoin();
     
+    /**
+     * Pseudorandomly picks a printing ASCII character. The space is considered 
+     * printing, the tab is not.
+     * @return An ASCII character. For example, 'm'.
+     */
     public abstract char nextASCIIChar();
     
+    /**
+     * Gives a pseudorandom sequence of printing ASCII characters of a specified 
+     * length. 
+     * @param length How long the sequence of ASCII characters should be. Should 
+     * not be negative. May be 0, but there's not much point to that. For 
+     * example, 7.
+     * @return A pseudorandom <code>String</code> with <code>length</code> ASCII 
+     * characters. For example, "t.lm8Q<". 
+     * @throws NegativeArraySizeException If <code>length</code> is negative.
+     */
     public abstract String nextASCIICharSeq(int length);
     
+    /**
+     * Gives a pseudorandom sequence of printing ASCII characters.
+     * @param minLength The minimum length of the sequence. May be 0 but must 
+     * not be negative. Must not be greater than <code>maxLength</code>. For 
+     * example, 3.
+     * @param maxLength The maximum length of the sequence. Must not be less 
+     * than <code>minLength</code>. For example, 7.
+     * @return A sequence of printing ASCII characters of at least 
+     * <code>minLength</code> but not more than <code>maxLength</code>. For 
+     * example, "_aZ6WW".
+     * @throws IllegalArgumentException If either <code>minLength</code> or 
+     * <code>maxLength</code> is negative, or if the former is greater than the 
+     * latter.
+     */
     public abstract String nextASCIICharSeq(int minLength, int maxLength);
     
+    // TODO: Remember how this was supposed to differ from nextASCIICharSeq
     public abstract String nextString();
     
     public abstract OffsetDateTime nextDateTime();
