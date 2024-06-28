d350494b4c6b0f4a28000e5b6fea48bcca380f4e
Alonso del Arte <alonso.delarte@gmail.com>
2024-06-28 00:08:26 -0400

    Passing test assert sets contain same but they do not though same size

--- src/org/testframe/api/Asserters.java
+++ src/org/testframe/api/Asserters.java
@@ -1913,12 +1913,20 @@ public class Asserters {
         //
     }
     
-    // TODO: Write tests for this
+    /**
+     * 
+     * @param <E>
+     * @param expected
+     * @param actual
+     * @param msg
+     */
     public static <E> void assertContainsSame(Set<E> expected, Set<E> actual, 
             String msg) {
+        Set<E> expSet = new HashSet<>(expected);
+        Set<E> actSet = new HashSet<>(actual);
         String errMsg = msg + ". Expected set to contain " + expected.toString() 
                 + " but actually contained " + actual.toString();
-        assert expected.size() == actual.size() : errMsg;
+        assert expSet.equals(actSet) : errMsg;
     }
     
     /**
