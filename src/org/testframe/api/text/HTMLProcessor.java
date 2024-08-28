783bb41b17f312099a602e3099181e65424a2e00
Alonso del Arte <alonso.delarte@gmail.com>
2024-08-28 01:18:40 -0400

    conformCharacterEntities( ) stub

--- src/org/testframe/api/text/HTMLProcessor.java
+++ src/org/testframe/api/text/HTMLProcessor.java
@@ -1,5 +1,10 @@
 package org.testframe.api.text;
 
 public class HTMLProcessor {
+    
+    // TODO: Write tests for this
+    public static String conformCharacterEntities(String s) {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
 
 }
